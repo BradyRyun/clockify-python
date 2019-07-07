fde70cb8a5fe7ccc893060b8030b8e511f49854e
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-07-07 08:56:37 -0300

    criando o settup da lib

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Clockify',  # Required
-    version='0.0.2',  # Required
-    packages=['Clockify'],
+    name='clockify',  # Required
+    version='0.0.3',  # Required
+    author="Paulo Sergio dos Santo Junior",
+    author_email="paulossjuniort@gmail.com",
+    description="A lib to access the Clockify ",
+ 
+    packages=find_packages(),
+    
+    install_requires=[
+        'requests'
+    ],
+
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    setup_requires=['wheel'],
     
 )
