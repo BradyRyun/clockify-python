8e7899ab57f2ffeecb02b8aa0408503ce01b1bc8
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-07-07 15:48:12 -0300

    x

--- teste.py
+++ teste.py
@@ -7,6 +7,7 @@ workspaces = clockify_server.get_all_workspaces()
 for workspace in workspaces:
     pprint (workspace.name)
     id = workspace.id
+    pprint (id)
     projects = clockify_server.get_all_projects(id)
     
     for project in projects:
