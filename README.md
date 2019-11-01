75eb7f496346d20521f6473154fe15097fc98af5
paulossjunior <paulossjunior@gmail.com>
2019-10-31 22:36:15 -0300

    atualização do readme

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-# Clockify
-Biblioteca customizada para acessar o Clockify.
+# Objetivo
+Biblioteca responsável por realizar operações (e.g., busca ou criação de projetos) no [clockify](https://clockify.me/).
 
-# Criando a lib
-python setup.py bdist_wheel
-python -m twine upload dist/*
-
+A biblioteca foi construída tomando como base a [documentação](https://clockify.me/developers-api) da ferramenta.
 
 ## Instalação
 pip install clockify
 
 
+## Exemplo de utilização
+
+```python
+from clockify.clockify import Clockify
+from pprint import pprint 
+clockify_server = Clockify("chavedeacesso")
+workspaces = clockify_server.get_all_workspaces()
+
+for workspace in workspaces:
+    pprint (workspace)
+```
--- teste.py
+++ teste.py
@@ -1,12 +1,22 @@
 from clockify.clockify import Clockify
 from pprint import pprint 
 clockify_server = Clockify("W/HtkbB5hxdlYvnp")
-
+import datetime 
 workspaces = clockify_server.get_all_workspaces()
 
 for workspace in workspaces:
-    pprint (workspace)
-    #users = clockify_server.get_all_workspace_users(workspace['id'])
-    #for user in users:
-    #    times = clockify_server.get_all_time_entry_user(workspace['id'], user['id'])
-    #    pprint (times)
+    #pprint (workspace)
+    users = clockify_server.get_all_workspace_users(workspace['id'])
+    for user in users:
+        pprint (user['name'])
+        pprint ("xxxxxxxxxxxxxxxx")
+        times = clockify_server.get_all_time_entry_user(workspace['id'], user['id'])
+        for time in times:
+            start = str(time['timeInterval']['start'])
+            end = str(time['timeInterval']['end'])
+            if start is not None:
+                start = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%SZ')
+            if end is not None:
+                end = datetime.datetime.strptime(end, '%Y-%m-%dT%H:%M:%SZ')
+            pprint (end -start)
+        
