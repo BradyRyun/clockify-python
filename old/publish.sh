b382c998c3206a4b305a2563db57271c110b4233
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2020-10-31 12:20:10 -0300

    redefinindo os pacotes

--- old/publish.sh
+++
@@ -1,3 +0,0 @@
-rm -rf dist build
-python setup.py bdist_wheel
-python -m twine upload dist/*
--- old/teste.py
+++
@@ -1,4 +0,0 @@
-from clockify.clockify import Clockify
-from pprint import pprint 
-clockify_server = Clockify("Xlb9JuSKM2s1TwAk")
-x = clockify_server.get_all_time_entry_user("5be0caaab079873ca2da70e5", "5be0caa9b079873ca2da70e2")
